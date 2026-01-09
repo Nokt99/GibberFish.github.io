5c37d590d7c141130e057d8e0aa7c95fae93b7d1
ian6207 <ianv6207@mytusd.org>
2026-01-08 19:24:12 -0800

    Add background image and titles to badBoy-mode
    Set background image and add titles for 'BAD BOY' mode.

--- Modes/badBoy-mode.js
+++ Modes/badBoy-mode.js
@@ -1,11 +1,16 @@
 //You know this kind of import and export, so do it on the top.
+document.body.style.backgroundImage = "url('https://img.freepik.com/premium-photo/cracked-wall-background-cracked-dry-ground_553949-249.jpg');";
 const Modtitle = document.createElement("h1");
 Modtitle.textContent = "BAD BOY";
+Modtitle.style.textAlign = "center";
 Modtitle.style.fontSize = "50px";
+Modtitle.style.fontFamily = "Impact";
+Modtitle.style.color = "red";
 const ModetitleDes = document.createElement("h1");
 ModtitleDes.textContent = "An added mod by Ian";
+ModtitleDes.textAlign = "center";
 
-const ModSTYLE = document.createElement("style");
-ModSTYLE.innerHTML = "bod
+document.body.appendChild(Modtitle);
+document.body.appendChild(ModtitleDes);
 
 //In Progress
