b20dc17b0bcc652a90eed6028b42985c11361900
GibberFish <zakariyah6204@mytusd.org>
2026-02-03 16:10:14 -0800

    Create defense.js

---
+++ backend/security/defense.js
@@ -0,0 +1,5 @@
+console.warn(STOP HACKING OR ELSE YOU GET HACKED);
+//after this ian, you can add a json code tht gets the hackers information and uploads it to local storeage
+
+//after message
+console.warn(you will be terminated if you dont stop);
